ce011fe367033f3df210da84cd0fbfdfb6a0a7ca
Pranav Rao <prra2413@tamu.edu>
2025-10-29 11:04:10 -0500

    fix

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"database"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"app"},{"l":"controllers"},{"l":"database"}];updateSearchResults();
